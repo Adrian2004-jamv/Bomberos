15500d42810dc4598a8fe80d8a47766e3498837d
PonchitoEC <cesar.unapucha6741@utc.edu.ec>
2026-08-19 14:09:43 -0500

    style: simplificar acciones a registro y nuevo

--- emergencias/tests.py
+++ emergencias/tests.py
@@ -356,7 +356,7 @@ class EmergenciasYDesplieguesTests(TestCase):
     def test_listado_ofrece_crear_emergencia_al_responsable(self):
         self.client.force_login(self.usuario_estacion)
         respuesta = self.client.get("/emergencias/")
-        self.assertContains(respuesta, "Nuevo incidente")
+        self.assertContains(respuesta, "> Nuevo<", html=False)
         self.assertNotContains(respuesta, "Administrar emergencias")
 
     def test_detalle_web_no_expone_emergencias_de_otra_institucion(self):
